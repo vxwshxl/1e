e2b98d5fc7a4e250cd26534ff5d523a1fb8bb184
vxwshxl <veebodosa@gmail.com>
2026-02-25 15:58:45 +0530

    v1

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
-// Allow opening side panel on extension icon click
-chrome.sidePanel
-    .setPanelBehavior({ openPanelOnActionIconClick: true })
-    .catch((error) => console.error(error));
-
 // Basic background tasks can be added here
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Galixent extension installed");
 });
+
+// Remove invalid behavioral config that was breaking the service worker
+// Setting default side panel is already done in manifest.json via action -> default_path
